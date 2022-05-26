ee10eef5daf6f47f89744127860f2098e1ae334e
Amanda Lima <51892828+AmandaLimasiva@users.noreply.github.com>
2022-05-26 19:44:29 -0300

    Alterações - OP

---
+++ script.js
@@ -0,0 +1,25 @@
+//FLOAT = 1.72
+//Double
+
+//Texto = String "Amanda"
+
+//num inteiro = integer 27
+
+//credito? - true ou false 
+
+const nameF = 'Amanda'
+
+const lastName = 'Lima'
+
+const age = 22
+
+console.log("Meu nome é  " + nameF  +  lastName + " Tenho " + age + "de idade")
+
+alert("Meu nome é  " + nameF  +  lastName + " Tenho " + age + "de idade")
+
+//Interpolação
+console.log(`Meu nome é ${nameF} ${lastName}, teno ${age} de idade!`)
+
+
+//Operadores aritmétricos
+
