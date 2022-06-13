5cafd779363e692aa969907959a3347f226b3b5e
Amanda Lima <51892828+AmandaLimasiva@users.noreply.github.com>
2022-06-13 19:51:42 -0300

    enquanto

--- aulaEnquanto.js
+++ aulaEnquanto.js
@@ -2,6 +2,8 @@ let soma = 0;
 
 let numero = -1;
 
+/*
+//WHILE
 while(numero !== 1){
     numero = parseInt(prompt("Digite um número: "))
 
@@ -9,3 +11,12 @@ while(numero !== 1){
 
     alert("A soma é: " + soma)
 }
+*/
+
+//DO WHILE
+do{
+    numero = parseInt(prompt("Digite um número: "))
+    soma = soma + numero;
+    alert("A soma é: " + soma)
+
+}while(numero !== -1)
